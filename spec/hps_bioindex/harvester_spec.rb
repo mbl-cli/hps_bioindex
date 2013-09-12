078602b47bc1bffd63295dce5d69c7d309bf43dc
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-09-12 18:39:22 -0400

    added tests for harvest

--- spec/hps_bioindex/harvester_spec.rb
+++ spec/hps_bioindex/harvester_spec.rb
@@ -2,31 +2,45 @@ require_relative '../spec_helper'
 
 describe HpsBioindex::Harvester do
   let(:harvester) { HpsBioindex::Harvester.new }
+  
   before(:all) { truncate_tables }
+
+  def get_item_file(request)
+    id = request.uri.path.match(%r|items/([\d]+)\.xml|)[1]
+    open(File.join(FILES_DIR, "item_%s.xml" % id))
+  end
 
   it 'should initiate' do
     harvester.class.should == HpsBioindex::Harvester
   end
 
   it 'should get updates from items of a community' do
-
-    any_instance_of(RestClient::Resource) do |klass|
-        stub(klass).get { ITEMS_ALL_6 }
-    end
-
+    stub_request(:get, %r|/rest/updates/|).to_return(
+      body: open(File.join(FILES_DIR, 'items_all_6.xml')))
     res = harvester.get_updates(community_id: 6)
     res.should_not be_nil
     res[:items_collection][:items].size.should == 31
   end
 
   it 'should store found items' do
-    # truncate_tables
-    # any_instance_of(RestClient::Resource) do |klass|
-    #     stub(klass).get { ITEMS_ALL_6 }
-    # end
-    # Item.count.should == 0
-    # harvester.harvest(community_id: 6)
-    # Item.count.should == 31
+    truncate_tables
+    
+    stub_request(:get, %r|/rest/updates/|).to_return(
+      body: open(File.join(FILES_DIR, 'items_all_6.xml')))
+    
+    stub_request(:get, %r|/rest/items/|).to_return do |request|
+      { body: get_item_file(request) }
+    end
+    
+    stub_request(:get, %r|/rest/bitstream/|).to_return do |request|
+      { body: open(File.join(FILES_DIR, 'bitstream_0.xhtml')) }
+    end
+    Item.count.should == 0
+    Bitstream.count.should == 0
+    harvester.harvest(community_id: 6)
+    Item.count.should == 31
+    Bitstream.count.should == 30
   end
-
 end
+
+  
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'coveralls'
 Coveralls.wear!
 
 require 'factory_girl'
+require 'webmock/rspec'
 
 ENV['DS_ENV'] = 'test'
 require_relative '../lib/hps_bioindex'
@@ -17,6 +18,7 @@ def truncate_tables
 end
 
 unless defined?(DS_CONSTANTS)
+  FILES_DIR = File.expand_path(File.join( File.dirname(__FILE__), 'files'))
   ITEMS_ALL_6 = File.read(File.join(File.dirname(__FILE__), 
                                     'files', 'items_all_6.xml'))
   ITEMS_20130805_6 = File.read(File.join(File.dirname(__FILE__), 
