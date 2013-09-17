45bce50d13e7fef0959a14a4a512edcfff7737bb
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-09-17 10:23:17 -0400

    env name changed

--- db/migrate/20130916202840_add_name_processed_to_bitstreams.rb
+++
@@ -1,6 +0,0 @@
-class AddNameProcessedToBitstreams < ActiveRecord::Migration
-  def change
-    add_column :bitstreams, :names_processed, :boolean
-    add_index :bitstreams, :names_processed, name: 'idx_bitstreams_1'
-  end
-end
